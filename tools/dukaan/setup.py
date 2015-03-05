04feeff31f1853d902738234440e7bfee0b0066f
dgabor89 <dgabor89@gmail.com>
2015-03-06 00:53:03 +0100

    Fixing the clone url for setup.py

--- tools/dukaan/setup.py
+++ tools/dukaan/setup.py
@@ -10,7 +10,7 @@ setup(name='dukaan',
 	description='Local testing tool for Windows Azure Store resource providers',
 	long_description=open('README.txt').read(),
 	license='LICENSE.txt',
-	url='https://github.com/WindowsAzure/azure-resource-provider-sdk/tree/master/tools/dukaan',
+	url='https://github.com/domonkosgabor/azure-resource-provider-sdk/tree/master/tools/dukaan',
 	packages=['dukaan'],
 	install_requires=['clint >= 0.3.1','iso8601 >= 0.1.4','requests >= 0.14.2'],
 	entry_points = {
